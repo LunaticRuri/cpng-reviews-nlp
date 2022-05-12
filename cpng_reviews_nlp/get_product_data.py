732d3ff6ec6a1765b6df3aafae96820625e5518e
Nuri Seok <105202566+LunaticRuri@users.noreply.github.com>
2022-05-12 11:18:00 +0900

    가독성 수정 및 progress bar 추가

--- cpng_reviews_nlp/get_product_data.py
+++ cpng_reviews_nlp/get_product_data.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from collections import OrderedDict
 import os
 import json
+from tqdm import tqdm
 
 # 아래 상수들은 실제 구현에서는 변경 여지 있음!
 MAX_THREADS = 30
@@ -15,16 +16,17 @@ REVIEWS_PATH = '../data/sample_reviews'
 
 
 def download_review_by_product(product_id):
-    # Headers에 User-Agent, Cookie 중 bm_sv와 x-coupang-accept-language, 그리고 Referer 있어야 함!
 
+    # Headers에 User-Agent, Cookie 중 bm_sv와 x-coupang-accept-language, 그리고 Referer 있어야 함!
     headers = {
         "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) '
                       'Version/15.4 Safari/605.1.15',
         'Cookie': 'Cookie:bm_sv=IHATECOOKIE;x-coupang-accept-language=ko_KR;',
-        'Referer': f'https://www.coupang.com/vp/products/{product_id}?isAddedCart='}
+        'Referer': f'https://www.coupang.com/vp/products/{product_id}?isAddedCart=',
+    }
 
     # product_name, category_id, category 가져오기
-    # category가 내부 카테고리가 아니라 외부 배치 기준
+    # category_id가 내부 카테고리 id가 아니라 외부 배치(링크에 표시된 것) 기준
 
     # Get product_name 이름 하나 가져오려고 전체를 파싱하는 비효율적 부분
     # TODO: 상품명 가져오는 경로 개선 생각
@@ -39,6 +41,7 @@ def download_review_by_product(product_id):
 
     # Get product_category
     product_category_url = f"https://www.coupang.com/vp/products/{product_id}/breadcrumb-gnbmenu"
+
     try:
         html = requests.get(product_category_url, headers=headers).text
         soup = BeautifulSoup(html, 'lxml')
@@ -51,6 +54,7 @@ def download_review_by_product(product_id):
         print(f"Http Error where {product_id}:", e)
         return False
 
+    # Ger reviews
     reviews = []
 
     # Ratings 1 to 5
@@ -58,9 +62,9 @@ def download_review_by_product(product_id):
 
         buffer_str = ""
         target_page = 1
-        char_count = 0
+        buffer_char_count = 0
 
-        while char_count < MAX_CHAR_COUNT:
+        while buffer_char_count < MAX_CHAR_COUNT:
             review_page_url = f'https://www.coupang.com/vp/product/reviews?productId={product_id}&page={str(target_page)}' \
                               f'&size=10&sortBy=ORDER_SCORE_ASC&ratings={str(ratings)}&q=&viRoleCode=3&ratingSummary=true'
             try:
@@ -76,19 +80,20 @@ def download_review_by_product(product_id):
             if soup.find("div", {"class": "sdp-review__article__no-review sdp-review__article__no-review--active"}):
                 break
 
+            # 리뷰 제목과 본문
             bodies = soup.find_all("div", {"class": ["sdp-review__article__list__headline",
                                    "sdp-review__article__list__review__content js_reviewArticleContent"]})
 
             buffer_str += "".join(" ".join(elem.text.split()) + SENTENCE_DSCR for elem in bodies).strip()
 
             target_page += 1
-            char_count = len(buffer_str)
+            buffer_char_count = len(buffer_str)
 
         time.sleep(0.1)
 
         reviews.append(buffer_str)
 
-    # serialize
+    # serialize_json
     json_data = OrderedDict()
     json_data["product_id"] = product_id
     json_data["product_name"] = product_name
@@ -99,23 +104,17 @@ def download_review_by_product(product_id):
         json.dump(json_data, make_file, ensure_ascii=False, indent=4)
 
 
-def download_stories(product_list):
+def download_products(product_list):
     threads = min(MAX_THREADS, len(product_list))
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-        executor.map(download_review_by_product, product_list)
+        futures = [executor.submit(download_review_by_product, elem) for elem in product_list.values()]
+
+        # tqdm progress bar
+        for _ in tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
+            pass
 
 
 if __name__ == '__main__':
-    print("concurrent.futures:")
-    t0 = time.time()
-    download_stories(['7958974', '130911332'])
-    t1 = time.time()
-    print(f"{t1 - t0} seconds to download {len([1, 2])} stories.")
+    download_products(['7958974', '130911332'])
 
-    print("normal method:")
-    t0 = time.time()
-    download_review_by_product('7958974')
-    download_review_by_product('130911332')
-    t1 = time.time()
-    print(f"{t1 - t0} seconds to download {len([1, 2])} stories.")
