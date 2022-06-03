8da70035139b9a1a3d0b7171dd056ab6c2d7d707
Nuri Seok <105202566+LunaticRuri@users.noreply.github.com>
2022-06-04 05:28:52 +0900

    소스 정리

--- cpng_reviews_nlp/main.py
+++ cpng_reviews_nlp/main.py
@@ -227,43 +227,48 @@ def test_simple_dependency_parser():
     print(dp.preprocessing_tuple(sentence))
 
 
-with open('../data/reviews/0abcdef.json') as fp:
-    products = json.load(fp)
+def new_sent_phrase_list():
+    with open('../data/reviews/0abcdef.json') as fp:
+        products = json.load(fp)
 
-sp = SimplePreprocessor()
+    sp = SimplePreprocessor()
 
-sent_phrase_list = []
+    sent_phrase_list = []
 
-for elem in tqdm(products.values(), total=len(products)):
-    reviews = elem['reviews']
+    for elem in tqdm(products.values(), total=len(products)):
+        reviews = elem['reviews']
 
-    positive_rv = ''
-    negative_rv = ''
+        positive_rv = ''
+        negative_rv = ''
 
-    for review in reviews:
-        if review['rating'] in ['1','2','3']:
-            negative_rv += review['data'] + '\n'
-        else:
-            positive_rv += review['data'] + '\n'
+        for review in reviews:
+            if review['rating'] in ['1', '2', '3']:
+                negative_rv += review['data'] + '\n'
+            else:
+                positive_rv += review['data'] + '\n'
 
-    min_len = min(len(positive_rv), len(negative_rv))
-    if min_len < 2000:
-        continue
+        min_len = min(len(positive_rv), len(negative_rv))
+        if min_len < 2000:
+            continue
 
-    positive_min_rv_list = positive_rv[:min_len].split('\n')
-    negative_min_rv_list = negative_rv[:min_len].split('\n')
+        positive_min_rv_list = positive_rv[:min_len].split('\n')
+        negative_min_rv_list = negative_rv[:min_len].split('\n')
 
-    for pos in positive_min_rv_list:
-        for p in sp.preprocessing_tuple(pos):
-            sent_phrase_list.append(['POS', ' '.join(p)])
-    for neg in negative_min_rv_list:
-        for n in sp.preprocessing_tuple(neg):
-            sent_phrase_list.append(['NEG', ' '.join(n)])
+        for pos in positive_min_rv_list:
+            for p in sp.preprocessing_tuple(pos):
+                sent_phrase_list.append(['POS', ' '.join(p)])
+        for neg in negative_min_rv_list:
+            for n in sp.preprocessing_tuple(neg):
+                sent_phrase_list.append(['NEG', ' '.join(n)])
 
-with open('sent_phrase_list.pickle', 'wb') as fp:
-    pickle.dump(sent_phrase_list, fp)
+    with open('sent_phrase_list.pickle', 'wb') as fp:
+        pickle.dump(sent_phrase_list, fp)
 
-"""x_train, x_test = train_test_split(sent_phrase_list, test_size=0.2)
+
+with open('sent_phrase_list.pickle', 'rb') as fp:
+    sent_phrase_list = pickle.load(fp)
+
+x_train, x_test = train_test_split(sent_phrase_list, test_size=0.2)
 
 print(len(x_train))
 print(len(x_test))
@@ -274,7 +279,7 @@ with open("phrase_sent_train.csv", "w", newline="") as f:
 with open("phrase_sent_test.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(x_test)
-"""
+
 
 
 
--- cpng_reviews_nlp/utils/sent_training_data.py
+++ cpng_reviews_nlp/utils/sent_training_data.py
@@ -1,250 +1,69 @@
-from konlpy.tag import Mecab
-from jamo import h2j, j2hcj
-from hangul_utils import join_jamos
 import json
-import re
+from tqdm import tqdm
+import pickle
+from sklearn.model_selection import train_test_split
+import csv
+from cpng_reviews_nlp.nlp.simple_preprocessor import SimplePreprocessor
 
 
-class SimplePreprocessor:
-    stop_pos = ['JX', 'JKS', 'JKC', 'JKG', 'JKO', 'JKB', 'JKV', 'JKQ', 'EF', 'IC', 'VCP', 'VCN', 'XSN', 'MAJ',
-                'VX', 'ETN', 'JC', 'EP', 'MM', 'NNBC', 'NP', 'EC', 'NMB', 'MAG', 'SY', 'SE', 'UNKNOWN']
+def new_sent_phrase_list():
+    with open('../data/reviews/0abcdef.json') as fp:
+        products = json.load(fp)
 
-    aspect_pos = ['NNP', 'NNG']
+    sp = SimplePreprocessor()
 
-    desc_pos = ['VV', 'VA', 'VP']  # VP는 '이다, 아니다'가 붙어 서술어 역할을 하는 것
+    sent_phrase_list = []
 
-    stop_word = []
+    for elem in tqdm(products.values(), total=len(products)):
+        reviews = elem['reviews']
 
-    pass_word = [['잘', 'MAG'], ['잘못', 'MAG']]
+        positive_rv = ''
+        negative_rv = ''
 
-    def __init__(self):
-        self.mecab = Mecab()
-
-    @staticmethod
-    def only_hangul_number_space_dot(raw_text):
-        r_hangul = re.sub(r'[^ㄱ-ㅣ가-힣ㅣ\s\d.]', "", raw_text)
-        return r_hangul
-
-    @staticmethod
-    def pos_remover(token_pos_tuple_list):
-        after_rm = []
-        is_mag_an = False
-
-        for elem in token_pos_tuple_list:
-
-            target_pair = list(elem)
-
-            if is_mag_an:
-                target_pair[0] = '안_' + target_pair[0]
-
-            is_etm = False
-            is_mag_an = False
-
-            if '+' in target_pair[1]:
-
-                pos_stack = target_pair[1].split('+')
-
-                if 'ETM' in pos_stack:
-                    orig_jamo_str = j2hcj(h2j(target_pair[0]))[:-1]
-                    target_pair[0] = join_jamos(orig_jamo_str)
-
-                    is_etm = True
-
-                if 'EC' in pos_stack:
-                    # 대충 잘라 내기
-                    if len(target_pair[0]) != 1:
-                        target_pair[0] = target_pair[0][:-1]
-
-                if 'XSV' in pos_stack:
-                    try:
-                        after_rm[-1][1] = 'VV'
-                    except IndexError:
-                        pass
-
-                if 'XSA' in pos_stack:
-                    try:
-                        after_rm[-1][1] = 'VA'
-                    except IndexError:
-                        pass
-
-                target_pair[1] = pos_stack[0]
-
-                if target_pair[1] not in SimplePreprocessor.stop_pos or \
-                        target_pair in SimplePreprocessor.pass_word:
-                    if is_etm:
-                        if target_pair[1] not in ['XSA', 'XSV']:
-                            after_rm.append(target_pair)
-
-                        after_rm.append(['*', 'ETM'])
-                    else:
-                        if target_pair[1] == 'ETM':
-                            after_rm.append(['*', 'ETM'])
-                        else:
-                            if target_pair[1] not in ['XSA', 'XSV']:
-                                after_rm.append(target_pair)
-
-
-
+        for review in reviews:
+            if review['rating'] in ['1', '2', '3']:
+                negative_rv += review['data'] + '\n'
             else:
-                if target_pair[1] == 'ETN':
-                    try:
-                        after_rm[-1][0] += target_pair[0]
-                        after_rm[-1][1] = 'NNG'
-                    except IndexError:
-                        pass
-                if target_pair[1] == 'XSV':
-                    try:
-                        after_rm[-1][1] = 'VV'
-                    except IndexError:
-                        pass
+                positive_rv += review['data'] + '\n'
 
-                if target_pair[1] == 'XSA':
-                    try:
-                        after_rm[-1][1] = 'VA'
-                    except IndexError:
-                        pass
+        min_len = min(len(positive_rv), len(negative_rv))
+        if min_len < 2000:
+            continue
 
-                if target_pair[1] == 'XSN':
-                    try:
-                        after_rm[-1][0] += target_pair[0]
-                    except IndexError:
-                        pass
+        positive_min_rv_list = positive_rv[:min_len].split('\n')
+        negative_min_rv_list = negative_rv[:min_len].split('\n')
 
-                if target_pair[1] == 'VCP':
-                    try:
-                        after_rm[-1][1] = 'VP'
-                    except IndexError:
-                        pass
+        for pos in positive_min_rv_list:
+            for p in sp.preprocessing_tuple(pos):
+                sent_phrase_list.append(['POS', ' '.join(p)])
+        for neg in negative_min_rv_list:
+            for n in sp.preprocessing_tuple(neg):
+                sent_phrase_list.append(['NEG', ' '.join(n)])
 
-                if target_pair[1] == 'VCN':
-                    try:
-                        after_rm[-1][1] = 'VP'
-                        after_rm[-1][0] += '_아니'
-                    except IndexError:
-                        pass
+    with open('sent_phrase_list.pickle', 'wb') as fp:
+        pickle.dump(sent_phrase_list, fp)
 
-                # 부정 부사 또는 보조 용언
-                if target_pair == ['안', 'MAG']:
-                    is_mag_an = True
+def write_train_test_csv():
+    with open('sent_phrase_list.pickle', 'rb') as fp:
+        sent_phrase_list = pickle.load(fp)
 
-                if target_pair == ['않', 'VX']:
-                    try:
-                        after_rm[-1][0] += '_않'
-                    except IndexError:
-                        pass
-                if target_pair == ['못하', 'VX']:
-                    try:
-                        after_rm[-1][0] += '_못'
-                    except IndexError:
-                        pass
+    x_train, x_test = train_test_split(sent_phrase_list, test_size=0.2)
 
-                if (target_pair[1] not in SimplePreprocessor.stop_pos and target_pair[1] not in ['XSA', 'XSV']) \
-                        or target_pair in SimplePreprocessor.pass_word:
-                    if target_pair[1] == 'ETM':
-                        after_rm.append(['*', 'ETM'])
-                    else:
-                        after_rm.append(target_pair)
+    print(len(x_train))
+    print(len(x_test))
 
-        after_rm.insert(0, ['', 'START'])
-        after_rm.append(['', 'END'])
-
-        return after_rm
-
-    @staticmethod
-    def remove_stop_word(token_pos_tuple_list):
-        return [elem for elem in token_pos_tuple_list if elem not in SimplePreprocessor.stop_word]
-
-    def preprocessing_str(self, pp_raw_text):
-        target_text = self.only_hangul_number_space_dot(pp_raw_text)
-        pos_list = self.pos_remover(self.mecab.pos(target_text))
-
-        filtered_pos_list = self.remove_stop_word(pos_list)
-        return ' '.join(str(e[0]) for e in filtered_pos_list)
-
-    def preprocessing_tuple(self, pp_raw_text):
-        target_text = self.only_hangul_number_space_dot(pp_raw_text)
-        pos_list = self.pos_remover(self.mecab.pos(target_text))
-
-        output_tuple_list = []
-        history = []
-
-        resv_indp = (False, ('', ''))
-
-        for elem in pos_list:
-            if elem == ['', 'START']:
-                history.insert(0, elem)
-            if elem[1] == 'SF':
-                history = ['', 'START']
-            elif elem[1] in SimplePreprocessor.aspect_pos or elem[1] == 'NNB':
-                # ETM ~한
-                if history[0][1] == 'ETM':
-                    tmp_stack = [history[1][0]]
-                    try:
-                        if history[2][1] in SimplePreprocessor.aspect_pos:
-                            tmp_stack.insert(0, history[2][0])
-                    except IndexError:
-                        pass
-                    if elem[1] == 'NNB':
-                        output_tuple_list.append(('', ' '.join(tmp_stack)))
-                    else:
-                        output_tuple_list.append((elem[0], ' '.join(tmp_stack)))
-
-                if history[0][1] in SimplePreprocessor.aspect_pos:
-                    output_tuple_list.append((history[0][0] + ' ' + elem[0], ''))
-
-            elif elem[1] in SimplePreprocessor.desc_pos:
-                if history[0][1] in SimplePreprocessor.aspect_pos:
-                    try:
-                        if history[1][1] in SimplePreprocessor.aspect_pos:
-                            output_tuple_list.append((history[1][0] + ' ' + history[0][0], elem[0]))
-                        else:
-                            output_tuple_list.append((history[0][0], elem[0]))
-                    except IndexError:
-                        pass
-                elif elem[1] == 'VA':
-                    resv_indp = (True, ('', elem[0]))
-
-            elif elem[1] != 'ETM' and resv_indp[0]:
-                output_tuple_list.append(resv_indp[1])
-                resv_indp = (False, ('', ''))
-
-            else:
-                pass
-
-            if elem != ['', 'START']:
-                history.insert(0, elem)
-
-        return output_tuple_list
+    with open("phrase_sent_train.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(x_train)
+    with open("phrase_sent_test.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(x_test)
 
 
-def test_simple_dependency_parser():
-    sentence = "가장 저렴한 라면."
-
-    dp = SimplePreprocessor()
-    print(dp.preprocessing_tuple(sentence))
 
 
-with open('../data/reviews/0abcdef.json') as fp:
-    products = json.load(fp)
 
-for elem in products.values():
-    reviews = elem['reviews']
 
-    positive_rv = ''
-    negative_rv = ''
-
-    for review in reviews:
-        if review['rating'] in ['1', '2', '3']:
-            negative_rv += review['data'] + '\n'
-        else:
-            positive_rv += review['data'] + '\n'
-
-    min_len = min(len(positive_rv), len(negative_rv))
-    if min_len < 2000:
-        continue
-
-    positive_min_rv = positive_rv[:min_len]
-    negative_min_rv = negative_rv[:min_len]
 
 
 
