f0d9c6e87b413d78f70853a140dec8fd5b239836
Nuri Seok <105202566+LunaticRuri@users.noreply.github.com>
2022-06-04 09:22:36 +0900

    소스 정리

--- cpng_reviews_nlp/main.py
+++ cpng_reviews_nlp/main.py
@@ -1,13 +1,1 @@
-# importing python package
-import pandas as pd
-
-# read contents of csv file
-file = pd.read_csv("phrase_sent_test.csv")
-
-# adding header
-headerList = ['Sent', 'Phrase']
-
-file = file[:200000]
-
-# converting data frame to csv
-file.to_csv("phrase_sent_test.csv", header=headerList, index=False)
+print("Hello, World!\n")
